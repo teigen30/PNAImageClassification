4323653b246011cd3cca38c2a3ff9b8b10ad6d77
JenSans <58453763+JenSans@users.noreply.github.com>
2021-11-18 15:59:55 -0500

    created another utils folder because the first utils folder doesn't like the new functions

--- utils.py
+++ utils.py
@@ -1,10 +1,22 @@
-from pathlib import Path
-import imagesize
-import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import plot_confusion_matrix
+import keras
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, Input, LSTM
+from keras.datasets import mnist
+from keras import regularizers, initializers, optimizers
+import os
+import datetime
+from keras.wrappers.scikit_learn import KerasRegressor
+from tensorflow.keras.callbacks import EarlyStopping
+from pathlib import Path
+import imagesize
 
 def pic_count(path, folder_name):
     ''' Returns a visualization of the picture counts for image folders by type.
@@ -25,7 +37,7 @@ def pic_count(path, folder_name):
                  height=500)
     fig.show()
     
-def plotly_eda(data, title1, title2, title3): 
+def eda(data, title1, title2, title3): 
     '''Returns three Plotly visuals including: 
     
     1. Scatter plot of Height and Width (pixels)
@@ -176,23 +188,20 @@ def plot_performance(hist):
     
     plt.show()
     
-def confusion_matrix(estimator, x, y):
+def confusion_matrix(model, x, y):
     """Returns a confusion matrix in Seaborn heatmap style.
     
-    estimator: model 
+    model: model 
     x: X_val (X validation) 
     y: y_val (y validation)
     
     """
-    y_predict_test = estimator.predict(x)
+    y_predict_test = model.predict(x)
     y_true_test = y
     res_test = tf.math.confusion_matrix(y_true_test, y_predict_test)
     res_test
     
     return sns.heatmap(res_test, annot=True, fmt='g')
-    
-import seaborn as sns
-import tensorflow as tf
 
 
 
